615327a5d4995100bdaf6fba694f2f5dc3613ecf
JustSong <songquanpeng@foxmail.com>
2023-04-24 21:57:56 +0800

    Adjust code style

--- websocket_thread.py
+++ websocket_thread.py
@@ -1,7 +1,7 @@
 import asyncio
+import datetime
 import json
 import ssl
-import datetime
 from threading import Thread, Event
 
 import websockets
@@ -28,11 +28,11 @@ class WebSocketThread(Thread):
 
     def show_message(self, message):
         self.main.tray_message_signal.emit(message['title'], message['description'])
-        timeVal = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        msgVal =timeVal+'\n'+message['title'] +'\n'+ message['description']+'\n'
+        time_val = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        msgVal = time_val + '\n' + message['title'] + '\n' + message['description'] + '\n'
         self.main.history_message_signal.emit(msgVal)
-        with open("history.txt", "a", encoding='utf-8') as file:
-            file.write(msgVal+'\n')
+        with open("messages.txt", "a", encoding='utf-8') as file:
+            file.write(msgVal + '\n')
 
     async def receive_message(self):
         try:
